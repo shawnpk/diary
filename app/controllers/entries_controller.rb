7131adf2613650359494ccb392b5aac1630fe0a4
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-07-31 12:33:23 -0700

    Added index and show actions and templates for diary entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,9 @@
 class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+  
+  def show
+    @entry = Entry.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resource :entries
+  resources :entries
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
