f3adf0098306eeed31ade7d3ead52b7f0acdc9a3
Shawn Kearney <shawnpk@ineptsoftware.com>
2016-07-31 12:59:43 -0700

    Added new, edit, create, update and delete actions and views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,51 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
   def index
-    @entries = Entry.all
+    @entries = Entry.all.order('created_at DESC')
   end
-  
+
   def show
-    @entry = Entry.find(params[:id])
+
   end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to entries_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @entry.update(entry_params)
+      redirect_to @entry
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @entry.delete
+    redirect_to entries_path
+  end
+
+  private
+    def entry_params
+      params.require(:entry).permit(:title, :contents)
+    end
+
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
 end
